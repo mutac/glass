4cb6abc0aa8c4e3d4d90d3830c6022254418fe95
Matt Waddell <mewtus@gmail.com>
2014-06-08 20:35:49 -0700

    Squashed commit of the following:
    commit de8b70b95ccf24b754229618ece50677788469b6
    Author: Matt Waddell <mewtus@gmail.com>
    Date:   Sun Jun 8 20:17:53 2014 -0700
    
        Added live video playback
    
    commit 4c8442dcac716cfa5c5398297c611c0bbd679af5
    Author: Matt Waddell <mewtus@gmail.com>
    Date:   Sat Jun 7 13:55:08 2014 -0700
    
        exclude eclipse workspace from checkin
    
    commit 33520511b4c626dcd85bf8b6f7ee32c1b20c5019
    Author: Matt Waddell <mewtus@gmail.com>
    Date:   Sat Jun 7 13:54:22 2014 -0700
    
        ...
    
    commit d29b858ec9db6c93ec1646526e92a4456992225f
    Author: Matt Waddell <mewtus@gmail.com>
    Date:   Sat Jun 7 13:53:33 2014 -0700
    
        ...

--- LiveVideo/src/cx/ath/there/android/glass/livevideo/MainActivity.java
+++ LiveVideo/src/cx/ath/there/android/glass/livevideo/MainActivity.java
@@ -2,7 +2,9 @@ package cx.ath.there.android.glass.livevideo;
 import cx.ath.there.android.glass.util.*;
 import android.app.Activity;
 import android.hardware.Camera;
+import android.media.CameraProfile;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -21,8 +23,31 @@ public class MainActivity
 {
 	public static String TAG = "LiveVideo";
 	
+	private Camera mCamera;
+	private boolean mIsCameraConfigured = false;
+	private boolean mIsVideoPlaying = false;
 	private TemperatureNotifier mTemperatureNotifier = new TemperatureNotifier();
 	private TextView mTemperatureText;
+	private SurfaceView mVideo;
+	private SurfaceHolder mVideoHolder;
+	
+    private SurfaceHolder.Callback mVideoSurfaceCallback = new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) 
+        {
+        	// nothing
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) 
+        {
+        	initVideo(width, height);
+        	startVideo();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) 
+        {
+            // nothing
+        }
+    };
 	
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -32,6 +57,10 @@ public class MainActivity
         setContentView(R.layout.fragment_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         
+        mVideo = (SurfaceView)findViewById(R.id.video);
+        mVideoHolder = mVideo.getHolder();
+        mVideoHolder.addCallback(mVideoSurfaceCallback);
+        
         mTemperatureText = (TextView)findViewById(R.id.temperature);
         mTemperatureNotifier.setTemperatureChangedListener(this);
     }
@@ -40,11 +69,18 @@ public class MainActivity
     public void onResume() 
     {
         super.onResume();
+        
+        mCamera = Camera.open();
     }
 
     @Override
     public void onPause() 
     {
+    	stopVideo();
+    	
+    	mCamera.release();
+    	mCamera = null;
+    	
         super.onPause();
     }
     
@@ -102,5 +138,62 @@ public class MainActivity
 	public void onTemperatureChanged(Temperature temperature)
 	{
 		mTemperatureText.setText("TEMP: " + temperature.toString());
+	}
+	
+	private boolean isVideoPlaying()
+	{
+		return isVideoConfigured() & mIsVideoPlaying;
+	}
+	
+	private boolean isVideoReady()
+	{
+		return mCamera != null && mVideoHolder.getSurface() != null;
+	}
+	
+	private boolean isVideoConfigured()
+	{
+		return mIsCameraConfigured && isVideoReady();
+	}
+	
+	private void initVideo(int width, int height)
+	{
+		if (isVideoReady())
+		{
+			try
+			{
+				mCamera.setPreviewDisplay(mVideoHolder);
+				
+			}
+			catch (Throwable t)
+			{
+				Log.e(TAG, "Exception in initVideo: ", t);
+			}
+			
+			if (!mIsCameraConfigured)
+			{
+				Camera.Parameters cameraParams = mCamera.getParameters();
+				cameraParams.setPreviewSize(width, height);
+				
+				mIsCameraConfigured = true;	
+			}
+		}
+	}
+	
+	private void startVideo()
+	{
+		if (isVideoConfigured())
+		{
+			mCamera.startPreview();
+			mIsVideoPlaying = true;
+		}
+	}
+	
+	private void stopVideo()
+	{
+		if (isVideoPlaying())
+		{
+			mCamera.stopPreview();
+			mIsVideoPlaying = false;
+		}
 	}
 }
